c1ec3314f8cc65cb2280edee1f78ebc459a8b160
Zach <2801818616z@gmail.com>
2025-12-12 18:00:45 +0800

    fix: redo
    Signed-off-by: Zach <2801818616z@gmail.com>

--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -5,44 +5,6 @@ export default function postPages() {
       <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
         This is the beginning of a new digital garden.
       </p>
-      <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        This is the beginning of a new digital garden.
-      </p>
-      <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        This is the beginning of a new digital garden.
-      </p>
-      <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        This is the beginning of a new digital garden.
-      </p>
-      <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        This is the beginning of a new digital garden.
-      </p>
-      <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        This is the beginning of a new digital garden.
-      </p>
-      <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        This is the beginning of a new digital garden.
-      </p>
-      <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        This is the beginning of a new digital garden.
-      </p>
-      <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        This is the beginning of a new digital garden.
-      </p>
-      <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        This is the beginning of a new digital garden.
-      </p>
-      <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        This is the beginning of a new digital garden.
-      </p>
-
-      <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        This is the beginning of a new digital garden.
-      </p>
-
-      <p className="text-lg text-gray-600 dark:text-gray-400 mt-4">
-        This is the beginning of a new digital garden.
-      </p>
     </div>
   )
 }
