a7dbc1260c847f524eb4028503e19e4025f9dc4c
Zach <2801818616z@gmail.com>
2025-12-14 13:40:35 +0800

    feat: introduce NavBar and Logo components and integrate NavBar into the root layout, hiding the home link on the root page.
    Signed-off-by: Zach <2801818616z@gmail.com>

---
+++ src/components/Logo.tsx
@@ -0,0 +1,8 @@
+export const Logo = () => {
+  return (
+    <>
+      <span>ZachSpace</span>
+      <span className="select-none animate-cursor--blink">__</span>
+    </>
+  )
+}
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,10 +1,17 @@
-import { Outlet, ScrollRestoration } from 'react-router'
+import { Outlet, ScrollRestoration, useLocation } from 'react-router'
 import { Footer } from '@/components/Footer'
+import { NavBar } from '@/components/NavBar'
 import { ReadableArea } from '@/components/ReadableArea'
 
 export default function RootLayout() {
+  const location = useLocation()
+  const isRootPage = location.pathname === '/'
+
   return (
     <>
+      <ReadableArea>
+        <NavBar hideHome={isRootPage} />
+      </ReadableArea>
       <ReadableArea className="mt-12 mb-20">
         <Outlet />
       </ReadableArea>
