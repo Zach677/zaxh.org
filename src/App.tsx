5060c4da5836478fe4190484cad37861172cb500
Zach <2801818616z@gmail.com>
2025-12-19 13:00:05 +0800

    feat: add Vercel Analytics and Speed Insights
    Signed-off-by: Zach <2801818616z@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { Suspense } from 'react'
 import { RouterProvider } from 'react-router'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 import { ThemeClientInitializer } from './theme'
 import { MetadataUpdater } from './metadata'
@@ -7,7 +9,7 @@ import { IntlProvider } from './components/IntlProvider'
 
 export const appMetadata = {
   title: 'ZachSpace',
-  description: "Zach's personal blog",
+  description: 'Zach\'s personal blog',
   url: 'https://zaxh.org',
 }
 
@@ -17,6 +19,8 @@ export function App(props: {
   const { router } = props
   return (
     <>
+      <Analytics />
+      <SpeedInsights />
       <ThemeClientInitializer />
       <MetadataUpdater router={router} initialMetadata={appMetadata} />
       <Suspense>
