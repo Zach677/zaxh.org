ed497d8e69e1e9e13c5d6e7706c70390919fde9c
Zach <2801818616z@gmail.com>
2025-12-21 18:21:51 +0800

    feat: add remark-gfm to enable GitHub Flavored Markdown
    Signed-off-by: Zach <2801818616z@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import mdx from '@mdx-js/rollup'
+import remarkGfm from 'remark-gfm'
 import rehypePrettyCode, { Options } from 'rehype-pretty-code'
 
 import { postProvider, pageProvider } from './plugins/content-provider'
@@ -17,6 +18,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
     mdx({
+      remarkPlugins: [remarkGfm],
       rehypePlugins: [
         [
           rehypePrettyCode,
